c4e5f6badd83b62d94632d15613226df050fca7c
= <=>
2023-02-21 19:56:13 -0400

    Change app titl

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ const App = () => {
 
   return (
     <div className="App">
-      <h1 className="app-title">Monsters Rolodex</h1>
+      <h1 className="app-title">Monsters Rolodex React</h1>
 
       <SearchBox
         onChangeHandler={onSearchChange}
